adec8dc7c49dc53aec2690fd953058734bf85483
Cristian Damil <cristiancode2023@gmail.com>
2024-10-14 00:27:13 +0200

    improve styles and refactors

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -16,6 +16,13 @@ export class CubeComponent implements OnInit {
 
   private params = this.route.params;
 
+  protected tiempo = 0;
+  private timerId: any;
+  protected timerActive = false;
+
+  protected times: any[] = [];
+  protected avg = "";
+
   ngOnInit(): void {
     this.params.subscribe((params) => {
       this.cube = Number(params["cube"]);
@@ -29,27 +36,6 @@ export class CubeComponent implements OnInit {
         this.router.navigate(["/3"]);
       }
     });
-  }
-
-  protected tiempo = 0;
-  private timerId: any;
-  protected timerActive = false;
-
-  protected times: any[] = [];
-  protected avg = 0;
-
-  protected startTimer() {
-    this.tiempo = 0;
-    this.timerActive = true;
-    this.timerId = setInterval(() => {
-      this.tiempo += 0.01;
-      this.tiempo = Number(this.tiempo.toFixed(2));
-    }, 10);
-  }
-
-  protected stopTimer() {
-    this.timerActive = false;
-    clearInterval(this.timerId);
   }
 
   @HostListener("window:keydown.space", ["$event"])
@@ -71,7 +57,20 @@ export class CubeComponent implements OnInit {
     this.tiempo = 0;
   }
 
+  protected startTimer() {
+    this.tiempo = 0;
+    this.timerActive = true;
+    this.timerId = setInterval(() => {
+      this.tiempo += 0.01;
+      this.tiempo = Number(this.tiempo.toFixed(2));
+    }, 10);
+  }
+
   protected saveTime() {
+    if (!this.tiempo) {
+      return;
+    }
+
     const date = new Date();
 
     const data = {
@@ -81,25 +80,33 @@ export class CubeComponent implements OnInit {
     };
 
     const id = new Date().getTime().toString();
-    if (this.tiempo) {
-      localStorage.setItem(id, JSON.stringify(data));
-      console.log("Data saved to local storage with id:", id, "Data:", data);
-      this.times.push(this.tiempo);
-      this.avg = this.media(this.times);
-    }
+
+    localStorage.setItem(id, JSON.stringify(data));
+
+    this.times.push(this.tiempo);
+    this.avg = this.average(this.times);
+
     this.stopTimer();
     this.tiempo = 0;
   }
 
-  private media(array: Object[]) {
+  protected stopTimer() {
+    this.timerActive = false;
+    clearInterval(this.timerId);
+  }
+
+  private average(array: Object[]) {
     let sum = 0;
+
     array.forEach((element: any) => {
       sum += element;
     });
+
     let average = 0;
     if (array.length > 0) {
       average = sum / array.length;
     }
-    return Number(average.toFixed(2));
+
+    return average.toFixed(2);
   }
 }
--- src/app/times/times.component.ts
+++ src/app/times/times.component.ts
@@ -24,11 +24,25 @@ export class TimesComponent implements OnInit {
   media3: any;
   media4: any;
 
-  openSnackBar(message: string, action: string): MatSnackBarRef<any> {
+  ngOnInit() {
+    this.localData = Object.values(localStorage).map((data) =>
+      JSON.parse(data)
+    );
+
+    this.resultados2 = this.localData.filter((cubo) => cubo.cubo === 2);
+    this.resultados3 = this.localData.filter((cubo) => cubo.cubo === 3);
+    this.resultados4 = this.localData.filter((cubo) => cubo.cubo === 4);
+
+    this.media2 = this.media(this.resultados2);
+    this.media3 = this.media(this.resultados3);
+    this.media4 = this.media(this.resultados4);
+  }
+
+  private openSnackBar(message: string, action: string): MatSnackBarRef<any> {
     return this._snackBar.open(message, action, { duration: 5000 });
   }
 
-  media(array: Object[]) {
+  protected media(array: Object[]) {
     let sum = 0;
     array.forEach((element: any) => {
       sum += element.tempo;
@@ -37,45 +51,7 @@ export class TimesComponent implements OnInit {
     return Number(average.toFixed(2));
   }
 
-  ngOnInit() {
-    const keys = Object.keys(localStorage);
-    console.log("Local storage data keys:", keys);
-
-    for (let i = 0; i < keys.length; i++) {
-      const key = keys[i];
-      const data = localStorage.getItem(key);
-      console.log("Local storage data for key", key, ":", data);
-      if (data) {
-        this.localData.push(JSON.parse(data));
-      } else {
-        console.log("No data found in local storage for key", key);
-      }
-    }
-
-    for (let cubo of this.localData) {
-      if (cubo.cubo === 2) {
-        this.resultados2.push(cubo);
-      }
-    }
-
-    for (let cubo of this.localData) {
-      if (cubo.cubo === 3) {
-        this.resultados3.push(cubo);
-      }
-    }
-
-    for (let cubo of this.localData) {
-      if (cubo.cubo === 4) {
-        this.resultados4.push(cubo);
-      }
-    }
-
-    this.media2 = this.media(this.resultados2);
-    this.media3 = this.media(this.resultados3);
-    this.media4 = this.media(this.resultados4);
-  }
-
-  downloadResultados4() {
+  protected downloadResultados4() {
     const worksheet = XLSX.utils.json_to_sheet(this.resultados4);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "4x4");
@@ -87,7 +63,7 @@ export class TimesComponent implements OnInit {
     saveAs(blob, "resultados4x4.xlsx");
   }
 
-  downloadResultados2() {
+  protected downloadResultados2() {
     const worksheet = XLSX.utils.json_to_sheet(this.resultados2);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "2x2");
@@ -99,7 +75,7 @@ export class TimesComponent implements OnInit {
     saveAs(blob, "resultados2x2.xlsx");
   }
 
-  downloadResultados3() {
+  protected downloadResultados3() {
     const worksheet = XLSX.utils.json_to_sheet(this.resultados3);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "3x3");
@@ -111,7 +87,7 @@ export class TimesComponent implements OnInit {
     saveAs(blob, "resultados3x3.xlsx");
   }
 
-  downloadTodo() {
+  protected downloadTodo() {
     const worksheet = XLSX.utils.json_to_sheet(this.localData);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Todo");
@@ -123,15 +99,16 @@ export class TimesComponent implements OnInit {
     saveAs(blob, "resultadosTodo.xlsx");
   }
 
-  parseAndFormatDate(dateString: string): string {
+  protected parseAndFormatDate(dateString: string): string {
     const parsedDate = new Date(dateString);
     return format(parsedDate, "dd MMM yyyy - HH:mm:ss (OOOO) ", { locale: gl });
   }
 
-  clearLocalStorage() {
+  protected clearLocalStorage() {
     const confirmDelete = window.confirm(
       "Estás seguro/a de que queres eliminar todos os tempos gardados? Esta acción non pode ser revertida"
     );
+
     if (confirmDelete) {
       localStorage.clear();
       console.log("Datos eliminados do teu almacenamento");
@@ -139,6 +116,7 @@ export class TimesComponent implements OnInit {
         "Datos eliminados correctamente",
         "OK"
       );
+
       snackBarRef.afterDismissed().subscribe(() => {
         this.router.navigate(["/"]);
       });
